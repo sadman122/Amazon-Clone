1b14c60a7e240294d61d48ff4c5e19ffd4674f5b
sadman122 <sadman.sakib122@gmail.com>
2025-12-08 17:35:12 -0800

    Add promises to code

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,8 +1,38 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { loadProducts } from "../data/products.js";
+import { loadCart } from "../data/cart.js";
 
+Promise.all([
+    new Promise((resolve) => {
+        loadProducts(() => {
+            resolve();
+        });
+    }),
+    new Promise((resolve) => {
+        loadCart(() => {
+            resolve();
+        });
+    })
+]).then(() => {
+    renderOrderSummary();
+    renderPaymentSummary();
+})
+
+/*
+new Promise((resolve) => {
+    loadProducts(() => {
+        resolve();
+    });
+}).then(() => {
+    renderOrderSummary();
+    renderPaymentSummary();
+});
+*/
+
+/*
 loadProducts(() => {
     renderOrderSummary();
     renderPaymentSummary();
 });
+*/
