b61ce7398acdb555c384bc3502448b2e8fc92a38
sadman122 <sadman.sakib122@gmail.com>
2025-12-09 17:54:05 -0800

    Create order using backend

--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -2,6 +2,7 @@ import { cart } from "../../data/cart.js";
 import { getProduct } from "../../data/products.js";
 import { deliveryOptions, getDeliveryOption } from "../../data/deliveryOptions.js";
 import { formatCurrency } from "../utils/money.js";
+import { addOrder } from "../../data/orders.js";
 
 export function renderPaymentSummary(){
     let productPriceCents = 0;
@@ -63,12 +64,35 @@ export function renderPaymentSummary(){
         </div>
     </div>
 
-    <button class="place-order-button button-primary">
+    <button class="place-order-button button-primary js-place-order">
         Place your order
     </button>
     `;
 
     document.querySelector('.js-payment-summary')
         .innerHTML = paymentSummaryHtml;
+
+    document.querySelector('.js-place-order')
+        .addEventListener('click', async () => {
+            try{
+                const response = await fetch('https://supersimplebackend.dev/orders', {
+                    method : 'POST',
+                    headers : {
+                        'Content-type' : 'application/json'
+                    },
+                    body : JSON.stringify({
+                        cart : cart
+                    })
+                });
+
+                const order = await response.json();
+                addOrder(order);
+
+            }catch(error){
+                console.log('Unexpected error. Please try again later.')
+            }
+
+            window.location.href = 'orders.html';
+        });
 
 }
